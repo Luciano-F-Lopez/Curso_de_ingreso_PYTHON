a037ccdce165b7b968f06b7e25c89d53a7acaf9c
Luciano-F-Lopez <lucianolopez717@gmail.com>
2024-02-05 15:29:01 -0300

    Ejercicio hasta el 5 resuelto

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -27,7 +27,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre_alumno = prompt ("nombre alumno","ingrese su nombre")
+        alert("nombre del alumno", nombre_alumno)
         
         
     
--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_05.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_05.py
@@ -40,9 +40,10 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
-
-        
+        nombre_alumno = self.txt_nombre.get ()
+        edad_alumno = self.txt_edad.get () 
+        datos= "Usted se llama " + nombre_alumno+ " y su edad es" + edad_alumno 
+        alert ("alumno", datos)
     
 if __name__ == "__main__":
     app = App()
